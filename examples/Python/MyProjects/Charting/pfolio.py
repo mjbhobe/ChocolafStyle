6eeb471cb52ebd5e2833fb2e4d8ea02c814e4e92
Manish Bhobe <mjbhobe@gmail.com>
2023-05-27 18:30:30 +0530

    Code Fixes

--- examples/Python/MyProjects/Charting/pfolio.py
+++ examples/Python/MyProjects/Charting/pfolio.py
@@ -10,12 +10,10 @@
 """
 import sys
 import os
-import argparse
 import pathlib
 from pathlib import Path
 
 from PyQt6.QtCore import *
-from PyQt6.QtGui import *
 from PyQt6.QtWidgets import *
 
 import yfinance
@@ -32,78 +30,41 @@ logger = chocolaf.get_logger(pathlib.Path(__file__).name)
 # some pandas tweaks
 pd.set_option("display.max_rows", 80)
 pd.set_option("display.max_columns", 50)
-pd.options.display.float_format = "{:,.2f}".format
+pd.options.display.float_format = '{:,.2f}'.format
 # pd.options.display.max_columns = 25
 pd.options.display.width = 1024
 
 # some global constants
 HOLDINGS = {
     "PFOLIO": [
-        "BAJAJ-AUTO.NS",
-        "BAJAJFINSV.NS",
-        "COLPAL.NS",
-        "DIXON.NS",
-        "HDFCBANK.NS",
-        "HEROMOTOCO.NS",
-        "HDFC.NS",
-        "INFY.NS",
-        "ITC.NS",
-        "KANSAINER.NS",
-        "LT.NS",
-        "M&M.NS",
-        "NESTLEIND.NS",
-        "PIDILITIND.NS",
-        "PGHH.NS",
-        "RELIANCE.NS",
-        "TCS.NS",
-        "TATASTEEL.NS",
-        "TITAN.NS",
-        "ULTRACEMCO.NS",
-    ],
+        'BAJAJ-AUTO.NS', 'BAJAJFINSV.NS', 'COLPAL.NS', 'DIXON.NS', 'HDFCBANK.NS', 'HEROMOTOCO.NS',
+        'HDFC.NS', 'INFY.NS', 'ITC.NS', 'KANSAINER.NS', 'LT.NS', 'M&M.NS', 'NESTLEIND.NS',
+        'PIDILITIND.NS', 'PGHH.NS', 'RELIANCE.NS', 'TCS.NS', 'TATASTEEL.NS', 'TITAN.NS',
+        'ULTRACEMCO.NS'],
     "NUM_SHARES": [
-        166,
-        530,
-        220,
-        25,
-        100,
-        25,
-        50,
-        832,
-        5000,
-        9900,
-        1080,
-        1440,
-        25,
-        7900,
-        50,
-        402,
-        408,
-        2500,
-        50,
-        62,
-    ],
+        166, 530, 220, 25, 100, 25, 50,
+        832, 5000, 9900, 1080, 1440, 25, 7900, 50,
+        402, 408, 2500, 50, 62]
 }
 
 todays_date = datetime.datetime.now()
 year, month, day = todays_date.year, todays_date.month, todays_date.day
 # adjust for financial year - if today() in Jan, Feb or Mar, decrease year by 1
-year = year - 1 if month in range(1, 4) else year
+year = (year - 1 if month in range(1, 4) else year)
 
 START_DATE = datetime.datetime(year, 4, 1)  # 01-Apr of current financial year
 END_DATE = datetime.datetime.now()
-print(
+logger.info(
     f"START_DATE = {START_DATE.strftime('%d-%b-%Y')} - END_DATE = "
-    f"{END_DATE.strftime('%d-%b-%Y')}"
-)
-locale.setlocale(locale.LC_MONETARY, "en_IN")
+    f"{END_DATE.strftime('%d-%b-%Y')}")
+
+# set to India locale
+locale.setlocale(locale.LC_ALL, 'en_IN.utf8')
 
 
 def download_stock_prices(
-    holdings=HOLDINGS,
-    start_date=START_DATE,
-    end_date=END_DATE,
-    save_path=None,
-    force_download=False,
+    holdings=HOLDINGS, start_date=START_DATE, end_date=END_DATE, save_path=None,
+    force_download=False
 ):
     if (save_path is not None) and (os.path.exists(save_path)) and (not force_download):
         # if portfolio was saved before, load from save_path (if exists) unless force_download
@@ -112,13 +73,13 @@ def download_stock_prices(
         logger.info(f"Portfolio loaded from {save_path}")
     else:
         # download stock prices
-        print("Downloading stock prices...")
+        logger.info("Downloading stock prices...")
         pfolio_df = pd.DataFrame()
         for symbol in holdings["PFOLIO"]:
             logger.info(f"Downloading {symbol} data from {start_date} to {end_date}...")
             stock_df = yfinance.download(symbol, start=start_date, end=end_date, progress=False)
             if len(stock_df) != 0:
-                pfolio_df[symbol] = stock_df["Close"]
+                pfolio_df[symbol] = stock_df['Close']
         pfolio_df.index = pd.to_datetime(pfolio_df.index)
         pfolio_df.index = pfolio_df.index.date
         # transpose so that stock names form the index
@@ -134,12 +95,12 @@ def download_stock_prices(
 def calculate_values(df, num_days=5):
     cols = df.columns
 
-    df_new = df[["Qty"]]
+    df_new = df[['Qty']]
 
     # calculate value of stocks for each day
     for col in cols[-num_days:]:
         df_new[col] = df[col]
-        df_new[f"{col}_Value"] = df[col] * df["Qty"]
+        df_new[f"{col}_Value"] = df[col] * df['Qty']
 
     # new_col_order = ['Qty']
     # for col in cols[1:]:
@@ -153,7 +114,8 @@ def calculate_values(df, num_days=5):
 def build_output(entry: Path, long: bool = False):
     if long:
         size = entry.stat().st_size
-        date = datetime.datetime.fromtimestamp(entry.stat().st_mtime).strftime("%d-%b-%Y %H:%M:%S")
+        date = datetime.datetime.fromtimestamp(entry.stat().st_mtime).strftime(
+            "%d-%b-%Y %H:%M:%S")
         type = "d" if entry.is_dir() else "f"
         return f"{type} {size:>10d} {date} {entry.name}"
     return entry.name
@@ -165,19 +127,20 @@ class PandasTableModel(QAbstractTableModel):
         self._data = data
 
     def data(self, index, role):
-        numRows = self.rowCount(0)
-
+        numRows = self.rowCount(0)  # any value for index is ok
         if index.row() == (numRows - 1):
-            value = ""  # default value, except for _Value cols
-            # I am on the Totals row, which does not provide data
-            # from the pandas dataframe, but is a calculated row
+            value = ""
+            # I am on the Totals row, which does not exist in the data!
+            # if column() is a "_Value" column, then calculate tota & display it
             colName = str(self._data.columns[index.column()]).strip()
             if colName.endswith("_Value"):
                 value = self._data[colName].sum()
-                print(f"Sum of column {colName} is {value:,.2f}", flush=True)
+                logger.info(f"Sum of column {colName} is {value:,.2f}")
         else:
-            # get value from pandas dataframe
+            # get value from dataframe
             value = self._data.iloc[index.row(), index.column()]
+
+        # value = self._data.iloc[index.row(), index.column()]
 
         if role == Qt.ItemDataRole.DisplayRole:
             # special formatting for floats, ints and dates
@@ -187,13 +150,12 @@ class PandasTableModel(QAbstractTableModel):
                 # display as YYYY-MM-DD
                 return value.strftime("%Y-%m-%d")
             if isinstance(value, float) or (value.dtype == np.float64):
-                # display in India currency format - all floats in
-                # dataframe are currency amounts
-                # @see: https://stackoverflow.com/questions/40951552/convert-an-amount-to-indian-notation-in-python
-                return locale.currency(value, grouping=True)
+                # format with comma & 2 decimal places
                 # return f"{value:,.2f}"
+                # format as currency
+                return locale.currency(value, grouping=True)
             if isinstance(value, int) or (value.dtype == np.int64):
-                # format with ,
+                # format with locale format
                 val = f"{value:,}"
                 return val
             # for all other cases, return value as-is
@@ -203,12 +165,8 @@ class PandasTableModel(QAbstractTableModel):
             # or isinstance(value, int):
             if isinstance(value, str):
                 return Qt.AlignmentFlag.AlignVCenter + Qt.AlignmentFlag.AlignLeft
-            elif (
-                isinstance(value, float)
-                or (value.dtype == np.float64)
-                or isinstance(value, int)
-                or (value.dtype == np.int64)
-            ):
+            elif isinstance(value, float) or (value.dtype == np.float64) \
+                or isinstance(value, int) or (value.dtype == np.int64):
                 return Qt.AlignmentFlag.AlignVCenter + Qt.AlignmentFlag.AlignRight
             else:
                 return Qt.AlignmentFlag.AlignVCenter + Qt.AlignmentFlag.AlignLeft
@@ -226,14 +184,10 @@ class PandasTableModel(QAbstractTableModel):
                 return "%s" % str(self._data.columns[section]).strip()
 
             if orientation == Qt.Orientation.Vertical:
-                return (
-                    "TOTAL VALUE"
-                    if (section == self.rowCount(0) - 1)
+                return "TOTAL VALUE" if (section == self.rowCount(0) - 1) \
                     else "%s" % str(self._data.index[section]).strip()
-                )
 
         # elif role == Qt.ItemDataRole.TextAlignmentRole:
-
     # if orientation == Qt.Orientation.Horizontal:
     #     return Qt.AlignmentFlag.AlignVCenter + Qt.AlignmentFlag.AlignRight
     # if orientation == Qt.Orientation.Vertical:
@@ -245,12 +199,13 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__()
         self.dataframe = dataframe
         self.tableView = QTableView()
-        self.tableView.horizontalHeader().setDefaultAlignment(Qt.AlignmentFlag.AlignHCenter)
+        self.tableView.horizontalHeader().setDefaultAlignment(
+            Qt.AlignmentFlag.AlignHCenter)
         self.model = PandasTableModel(self.dataframe)
         self.tableView.setModel(self.model)
         self.tableView.horizontalHeader().setSectionResizeMode(
-            QHeaderView.ResizeMode.ResizeToContents
-        )
+            QHeaderView.ResizeMode.ResizeToContents)
+
         self.setCentralWidget(self.tableView)
 
 
@@ -270,11 +225,12 @@ if __name__ == "__main__":
     save_path = Path(__file__).absolute().parents[0] / "pfolio" / f"pfolio_{today}.csv"
 
     # pfolio_df = download_stock_prices().T
-    pfolio_df = download_stock_prices(start_date=START_DATE, end_date=END_DATE, save_path=save_path)
+    pfolio_df = download_stock_prices(start_date=START_DATE, end_date=END_DATE,
+                                      save_path=save_path)
     # pfolio_df['Qty'] = HOLDINGS["NUM_SHARES"]
-    print(pfolio_df.iloc[:, -5:].head())
+    logger.info(pfolio_df.iloc[:, -5:].head())
     df_values = calculate_values(pfolio_df, 5)
-    print(df_values)
+    logger.info(df_values)
     save_path = Path(__file__).absolute().parents[0] / "pfolio" / f"pfolio_{today}_vals.csv"
     df_values.to_csv(save_path, index=True, header=True)
 
